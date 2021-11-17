44a948986007dab06b590e0f6529907db629d503
araff-16 <alex_raffa16@hotmail.com>
2021-11-17 22:48:21 +0000

    Refetch tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,7 +38,7 @@ const createTweetElement = function(tweetInfo) {
 
 const renderTweets = function(tweets) {
   for (let tweetdata of tweets) {
-    $('#tweet-container').append(createTweetElement(tweetdata));
+    $('#tweet-container').prepend(createTweetElement(tweetdata));
   }
 };
 
@@ -48,21 +48,29 @@ $(document).ready(function() {
   $('#new-tweet').on('submit', (evt) => {
     evt.preventDefault();
     
-    if (evt.target.text.value === ''){
-      alert("Tweets cannot be an empty string")
-      return 
+    if (evt.target.text.value === '') {
+      alert("Tweets cannot be an empty string");
+      return;
     }
 
-    if (evt.target.counter.value < 0){
-      alert("Tweets cannot be greater than 140 characters")
-      return 
+    if (evt.target.counter.value < 0) {
+      alert("Tweets cannot be greater than 140 characters");
+      return;
     }
 
     const val = $(evt.target.text).serialize();
     
     $.post("/tweets/", val).then(()=>{
+      
       //Resets the field in form
       $('#new-tweet').trigger("reset");
+
+      //Make new AJAX to load new tweet
+      $.get("/tweets").then(data => {
+        let newTweet = data[data.length - 1];
+        $('#tweet-container').prepend(createTweetElement(newTweet));
+      });
+
     });
 
   });
