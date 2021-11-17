f1387099f42b896a9aab7ece7770df981c78d1d7
araff-16 <alex_raffa16@hotmail.com>
2021-11-17 01:04:10 +0000

    complete tweet article

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  console.log('BANANA')
+
+  $("#tweetinput").on('input',function() {
+
+    //NOTE WE NEED TO USE $(this) to access the jquery methods
+    let counter = 140 - $(this).val().length;
+    
+
+
+    const countobj = $(this).closest('.new-tweet').find(".counter")
+    countobj.text(counter)
+
+    if (counter < 0) {
+      countobj.css("color", "red")
+    } else {
+      countobj.css("color", "#545149")
+    }
+    
+  })
+
+
+});
